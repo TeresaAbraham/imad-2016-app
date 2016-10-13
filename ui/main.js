7ca89bcbf6bdf3714879c3d3f0d3915f74afe399
TeresaAbraham <teresaabraham203@gmail.com>
2016-10-13 15:10:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,17 @@
-console.log('Loaded!');
+// Counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
-// Change the text of the main-text div
+button.onclick = function (){
+   
+   // make a request to the counter endpoint
+   
+   //capture the request and store it in a variable
+   
+   //render the variable in the correct span
+   counter = counter+ 1;
+   var span = document.getElementById('count');
+   span.innerHTML = counter.toString();
 
-var element = document.getElementById('main-text');
-
-element.innerHTML = "Teresa Abraham" ;
-
-// Move the image
-
-var img = document.getElementById('madi');
-var marginLeft = 0 ;
- function moveRight () {
-     marginLeft = marginLeft + 1 ;
-     img.style.marginLeft = marginLeft + 'px'  ;
-}
-
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50) ;
     
 };
-
