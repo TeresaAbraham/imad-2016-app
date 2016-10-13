9e4f696b71c0d9669a91946cefb365ba510c2963
TeresaAbraham <teresaabraham203@gmail.com>
2016-10-13 13:46:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,8 +11,8 @@ element.innerHTML = "Teresa Abraham" ;
 var img = document.getElementById('madi');
 var marginLeft = 0 ;
  function moveRight () {
-     marginLeft = marginLeft = 1 ;
-     img.style.marginLeft = marginLeft = 'px'  ;
+     marginLeft = marginLeft + 1 ;
+     img.style.marginLeft = marginLeft + 'px'  ;
 }
 
 img.onclick = function () {
