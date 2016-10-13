d82970123a3f57c58ffeb21d07230b04d6d7b559
TeresaAbraham <teresaabraham203@gmail.com>
2016-10-13 15:41:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,14 +4,24 @@ var counter = 0;
 
 button.onclick = function (){
    
-   // make a request to the counter endpoint
+   // create a request 
+   var request = new XMLHttpRequest ();
    
    //capture the request and store it in a variable
-   
-   //render the variable in the correct span
-   counter = counter+ 1;
-   var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
-
+   request.onreadystatechange = function (){
+     if (request.readyState ===XMLHttpRequest.DONE) {
+         //Take some Action
+         if (reqest.status === 200){
+             var counter = request.responseText;
+             var span = document.getElementById('count');
+             span.innerHTML = counter.toString();
+        }
+     }  
+     //Not done yet
+   };
+  
+  //make the Request
+   request.open('GET','http://teresaabraham.imad.hasura-app.io/counter', true);
+   request.send(null);
     
 };
