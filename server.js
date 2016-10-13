cc203d0df2f92cfc699f140c5d3178f32ac5b0b2
TeresaAbraham <teresaabraham203@gmail.com>
2016-10-13 07:32:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+    title: 'Article One | Teresa',
+    heading: 'Article One',
+    date: '12th October 2016',
+    content: `        <p>
+                            this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of 
+                       </p>
+                        <p>
+                             this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the  content of my first article.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of 
+                       </p>
+                         <p>
+                              this is the content of my first article.this is the content of my first article.this is the content of my first article.this is  the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of.this is the content of my first article.this is the content of my first article.this is the content of my first article.this is the content of 
+                        </p> `
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+`</html>
+    <head>
+        <title>
+                   ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+        
+    </head>
+    <body>
+        <div class="container">
+        
+             <div>
+                 <a href="/"> Home </a>
+             </div>
+                 <hr/> 
+                 <h3>
+                    ${heading}
+                 </h3>
+             <div>
+                    ${date}
+             </div>
+             <div>
+                    ${content}
+             </div> 
+        </div>
+    </body>
+</html>
+ `;
+ return htmlTemplate;
+}
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function (req, res){
      res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
